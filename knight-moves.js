21f37bbd9f8611b19d98ef530370ce509d5a418e
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-07-18 20:30:30 +0800

    Add info on how to create it

---
+++ knight-moves.js
@@ -0,0 +1,24 @@
+/**
+ * you need to think about the rules of the board and knight,
+ * and remember to follow them.
+ *
+ *
+ * answer => the rule of the board would be a road for the knight to take
+ *        => the rule of the knight is to like an L
+ */
+
+/**
+ * for every square there is a number of possible moves,
+ * don't make a move that can go over the board.
+ *
+ * array = [ ];
+ * **/
+
+/**
+ * decide what search algorithm to use, remember that
+ * one of them could be potentially infinite series
+ */
+
+/**
+ * use the search algorithm and output every path it would take
+ */
