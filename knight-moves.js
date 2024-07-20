bfcc5ef13643712e5c30f2fe0068b5c76751646b
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-07-20 21:04:36 +0800

    Create knight chess Graph using all the small methods such
    create the chess board coordinate
    get every knight moves
    remove the knight moves that go over the chess board
    translate coordinate into a number
    use merge sort to sort the array of numbers
    use binary search tree to build this sorted array number
    assign squares to binary tree

--- knight-moves.js
+++ knight-moves.js
@@ -1,24 +1,99 @@
-/**
- * you need to think about the rules of the board and knight,
- * and remember to follow them.
- *
- *
- * answer => the rule of the board would be a road for the knight to take
- *        => the rule of the knight is to like an L
- */
+import translate2DIntoNum from "./js/translate-coordinate-to-num.js";
+import binarySearchTree from "./js/binary-search-tree.js";
+import mergeSort from "./js/merge-sort.js";
+import prettyPrint from "./js/pretty-print.js";
 
-/**
- * for every square there is a number of possible moves,
- * don't make a move that can go over the board.
- *
- * array = [ ];
- * **/
+const createChessCoordinates = (
+  frontLeftCoordinate = 7,
+  frontRightCoordinate = 0
+) => {
+  const chessBoardCoordinates = [];
+
+  let coordinate = [frontLeftCoordinate, frontRightCoordinate];
+
+  if (frontLeftCoordinate === 0 && frontRightCoordinate === 7) {
+    return [[0, 7]];
+  }
+
+  if (frontRightCoordinate !== 8) {
+    frontRightCoordinate++;
+  }
+
+  if (frontRightCoordinate === 8) {
+    frontRightCoordinate = 0;
+    frontLeftCoordinate--;
+  }
+
+  return chessBoardCoordinates.concat(
+    [coordinate],
+    createChessCoordinates(frontLeftCoordinate, frontRightCoordinate)
+  );
+};
+
+const getKnightMoves = (coordinate) => {
+  let coor = coordinate;
+
+  return [
+    [coor[0] + 2, coor[1] - 1],
+    [coor[0] + 2, coor[1] + 1],
+    [coor[0] + 1, coor[1] + 2],
+    [coor[0] + 1, coor[1] - 2],
+    [coor[0] - 2, coor[1] - 1],
+    [coor[0] - 2, coor[1] + 1],
+    [coor[0] - 1, coor[1] - 2],
+    [coor[0] - 1, coor[1] + 2],
+  ];
+};
+
+const removeOverTheBoardMove = (knightPossibleMoves) => {
+  const moves = knightPossibleMoves;
+  return moves.filter(
+    (element) =>
+      element.at(0) >= 0 &&
+      element.at(0) <= 7 &&
+      element.at(1) >= 0 &&
+      element.at(1) <= 7
+  );
+};
+
+const bst = binarySearchTree();
+
+const translateAllCoordinatesToNumbers = (arrayOfCoordinates) => {
+  let array = arrayOfCoordinates;
+  return array.map((element) => translate2DIntoNum(element));
+};
+
+const createChessBoardGraphForKnight = () => {
+  let squares = [];
+
+  createChessCoordinates().forEach((element, index) => {
+    let item = element;
+
+    const knightMovements = getKnightMoves(item);
+    const filteredKnightMovements = removeOverTheBoardMove(knightMovements);
+
+    const arrayOfIndexesFilteredKnightMovements =
+      translateAllCoordinatesToNumbers(filteredKnightMovements);
+
+    const sortArrayOfIndexesFilteredKnightMovements = mergeSort(
+      arrayOfIndexesFilteredKnightMovements,
+      0,
+      arrayOfIndexesFilteredKnightMovements.length - 1
+    );
+
+    squares[index] = bst.buildTree(
+      sortArrayOfIndexesFilteredKnightMovements,
+      0,
+      sortArrayOfIndexesFilteredKnightMovements.length - 1
+    );
+  });
+
+  return squares;
+};
+
+const knightMoves = (from, to) => {};
 
 /**
  * decide what search algorithm to use, remember that
  * one of them could be potentially infinite series
- */
-
-/**
- * use the search algorithm and output every path it would take
  */
