b9051f31ca0516ce191ce260f9d1574b136934b7
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-07-23 14:16:48 +0800

    Get the index of the coordinate

--- js/translate-coordinate-to-num.js
+++ js/translate-coordinate-to-num.js
@@ -1,44 +1,20 @@
-/**
- * @pseudocode
- * todo: create a function that translate the coordinate into a number
- *  ? translate2dintoNum([7, 0]) === 0
- *  ? translate2dIntoNum([3, 3]) === 36
- */
+import chessCoordinates from "./chess-coordinates.js";
 
 const translate2DIntoNum = (coordinate) => {
-  let frontCoordinate = [7, 0];
-  let index = 0;
-  const coor = coordinate;
+  let chessCoor = chessCoordinates();
+  let [front, end] = coordinate;
+  let num = 0;
 
-  if (
-    frontCoordinate.at(0) === coor.at(0) &&
-    frontCoordinate.at(1) === coor.at(1)
-  ) {
-    return index;
-  }
+  chessCoor.forEach((element, index) => {
+    let [first, second] = element;
 
-  for (let i = 1; i < 64; i++) {
-    if (
-      frontCoordinate.at(0) === coor.at(0) &&
-      frontCoordinate.at(1) === coor.at(1)
-    ) {
-      index = i;
+    if (first === front && end === second) {
+      num = index;
+      return num;
     }
+  });
 
-    if (i % 8 === 0) {
-      frontCoordinate[0]--;
-    }
-
-    if (frontCoordinate.at(1) !== 8) {
-      frontCoordinate[1]++;
-    }
-
-    if (frontCoordinate.at(1) === 8) {
-      frontCoordinate[1] = 0;
-    }
-  }
-
-  return index;
+  return num;
 };
 
 export default translate2DIntoNum;
