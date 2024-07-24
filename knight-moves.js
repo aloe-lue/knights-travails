a6ac58ddfd79c39cbd646d282e7da308fb796747
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-07-24 20:44:19 +0800

    Create createPath method that returns an array
    returns an array of objects that contains from position and it's moves
    i thought that i have to make this because i could find the shortest
    path

--- knight-moves.js
+++ knight-moves.js
@@ -1,84 +1,34 @@
-import translate2DIntoNum from "./js/translate-coordinate-to-num.js";
-import binarySearchTree from "./js/binary-search-tree.js";
-import mergeSort from "./js/merge-sort.js";
+import chessBoardGraph from "./js/chess-board-graph.js";
 import prettyPrint from "./js/pretty-print.js";
-
-const createChessCoordinates = (
-  frontLeftCoordinate = 7,
-  frontRightCoordinate = 0
-) => {
-  const chessBoardCoordinates = [];
-
-  let coordinate = [frontLeftCoordinate, frontRightCoordinate];
-
-  if (frontLeftCoordinate === 0 && frontRightCoordinate === 7) {
-    return [[0, 7]];
-  }
-
-  if (frontRightCoordinate !== 8) {
-    frontRightCoordinate++;
-  }
-
-  if (frontRightCoordinate === 8) {
-    frontRightCoordinate = 0;
-    frontLeftCoordinate--;
-  }
-
-  return chessBoardCoordinates.concat(
-    [coordinate],
-    createChessCoordinates(frontLeftCoordinate, frontRightCoordinate)
-  );
-};
-
-const getKnightMoves = (coordinate) => {
-  let coor = coordinate;
-
-  return [
-    [coor[0] + 2, coor[1] - 1],
-    [coor[0] + 2, coor[1] + 1],
-    [coor[0] + 1, coor[1] + 2],
-    [coor[0] + 1, coor[1] - 2],
-    [coor[0] - 2, coor[1] - 1],
-    [coor[0] - 2, coor[1] + 1],
-    [coor[0] - 1, coor[1] - 2],
-    [coor[0] - 1, coor[1] + 2],
-  ];
-};
-
-const removeOverTheBoardMoves = (knightPossibleMoves) => {
-  const moves = knightPossibleMoves;
-  return moves.filter(
-    (element) =>
-      element.at(0) >= 0 &&
-      element.at(0) <= 7 &&
-      element.at(1) >= 0 &&
-      element.at(1) <= 7
-  );
-};
+import binarySearchTree from "./js/binary-search-tree.js";
+import translate2DIntoNum from "./js/translate-coordinate-to-num.js";
+import translateIndexToCoordinate from "./js/translate-num-to-coordinate.js";
 
 const bst = binarySearchTree();
 
-const translateAllCoordinatesToNumbers = (arrayOfCoordinates) => {
-  let array = arrayOfCoordinates;
-  return array.map((element) => translate2DIntoNum(element));
+const createPath = (from, to) => {
+  const chessGraph = chessBoardGraph();
+  let curr = translate2DIntoNum(from);
+  let newLoc = translate2DIntoNum(to);
+
+  let queue = [];
+  queue.push(curr);
+  let broadPath = [];
+
+  while (queue.length !== 0) {
+    let front = queue.at(0);
+
+    let currSquare = chessGraph[front];
+    let moves = bst.levelOrder(currSquare);
+    let indexes = moves.map((element) => queue.push(element));
+
+    broadPath.push({ position: queue.shift(), moves });
+
+    if (front === newLoc) {
+      return broadPath;
+    }
+  }
+  return broadPath;
 };
 
-const createChessBoardGraphForKnight = () => {
-  const chessCoordinates = createChessCoordinates();
-
-  return chessCoordinates.map((element) => {
-    let item = element;
-    const knightMovements = getKnightMoves(item);
-    const removeMoves = removeOverTheBoardMoves(knightMovements);
-    const translateToNum = translateAllCoordinatesToNumbers(removeMoves);
-    const sortMoves = mergeSort(translateToNum, 0, translateToNum.length - 1);
-    item = bst.buildTree(sortMoves, 0, sortMoves.length - 1);
-    return item;
-  });
-};
-
-const graphChess = createChessBoardGraphForKnight();
-
-const knightMoves = (from, to) => {
-  // translate the from to num and to to num
-};
+console.log(createPath([3, 3], [4, 3]), translate2DIntoNum([4, 3]));
