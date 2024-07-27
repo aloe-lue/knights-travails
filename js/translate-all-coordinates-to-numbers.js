99509a6e13f0e760b466c5b2a092d19898e9972b
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-07-27 18:15:43 +0800

    Do this things too

--- js/translate-all-coordinates-to-numbers.js
+++
@@ -1,8 +0,0 @@
-import translate2DIntoNum from "./translate-coordinate-to-num.js";
-
-const translateAllCoordinatesToNumbers = (arrayOfCoordinates) => {
-  let array = arrayOfCoordinates;
-  return array.map((element) => translate2DIntoNum(element));
-};
-
-export default translateAllCoordinatesToNumbers;
