555f2562c729148ba3ecdd47997408b2ea7ebef2
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-07-29 18:25:52 +0800

    Learn dijkstras algorithm to find the shortest path
    i couldn't find a solution on my own so i search something interesting
    to learn about this

--- knight-moves.js
+++ knight-moves.js
@@ -1,39 +1,5 @@
-import chessBoardGraph from "./js/chess-board-graph.js";
-import prettyPrint from "./js/pretty-print.js";
-import binarySearchTree from "./js/binary-search-tree.js";
-import translate2DIntoNum from "./js/translate-coordinate-to-num.js";
-import translateIndexToCoordinate from "./js/translate-num-to-coordinate.js";
+import chessBoardGraph from "./chess-board-graph.js";
 
-const bst = binarySearchTree();
-
-const createPath = (from, to) => {
-  const chessGraph = chessBoardGraph();
-  const current = translate2DIntoNum(from);
-  const newCurr = translate2DIntoNum(to);
-
-  const queue = [];
-  const path = [];
-  queue.push(current);
-
-  while (queue.length !== 0) {
-    let front = queue.at(0);
-    let bstMoves = chessGraph.at(front);
-    let movesArr = bst.inOrder(bstMoves);
-
-    movesArr.forEach((element) => {
-      let e = element;
-      queue.push(e);
-    });
-
-    path.push({ position: front, moves: movesArr });
-
-    if (bst.find(bstMoves, newCurr)) {
-      return path;
-    }
-
-    queue.shift();
-  }
-
-};
-
-console.log(createPath([0, 1], [0, 0]));
+// i have to learn dijkstra's algorithm
+// i think i have to start over
+const chessGraph = chessBoardGraph();
