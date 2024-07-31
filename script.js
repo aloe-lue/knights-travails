b0e3c6d20cbf8f63142e4f71439967c69b7bc5b2
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-07-31 18:14:39 +0800

    Make a script that runs the knight moves to test it out

---
+++ script.js
@@ -0,0 +1,11 @@
+import knightMoves from "./knight-moves.js";
+
+knightMoves([0, 0], [3, 3]);
+
+knightMoves([0, 0], [7, 7]);
+
+knightMoves([0, 1], [0, 0]);
+
+knightMoves([7, 0], [7, 7]);
+
+knightMoves([3, 3], [7, 7]);
